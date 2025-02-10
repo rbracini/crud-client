58aa09cb07769170c430195a6b1dbdd734b6510d
Regis Bracini <regis.bracini@gmail.com>
2025-02-10 17:55:59 -0300

    Setup inicial

---
+++ src/test/java/tech/curtiu/crud_client/CrudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.curtiu.crud_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
